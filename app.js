85e7db5ad67e7ed8fd5970bfe3e48e4caec6ce24
parker3216 <parker3216@hotmail.com>
2022-08-04 11:32:04 +0800

    feat add mongoose

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
+// 載入 express 並建構應用程式伺服器
 const express = require('express')
+const mongoose = require('mongoose') // 載入 mongoose
+mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error',() =>{
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () =>{
+  console.log('mongodb connected!')
+})
+
+
+
 const app = express()
 const port = 3000
 
---
+++ models/restaurant.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose') //載入mongoose
+const Schema = mongoose.Schema  //執行mongoose.Schema模組
+const restaurantSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  name_en: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  google_map: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  }
+})
+  module.exports = mongoose.model('Restaurant', restaurantSchema) //匯出資料
