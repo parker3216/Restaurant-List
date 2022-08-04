ec6365ef8df1e241d8ac4da12ec3c07501c5afc9
parker3216 <parker3216@hotmail.com>
2022-08-04 12:15:49 +0800

    feat:add restaurant seeder

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose') // 載入 mongoose
+const Restaurant = require('../restaurant') // 載入 restaurant model
+const restaurantList = require("../../restaurant.json").results //載入清單
+
+mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB
+
+
+const db = mongoose.connection // 取得資料庫連線狀態
+
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+// 連線成功
+db.once('open', () => {
+  console.log('loading restaurantSeeder data!')
+  
+  Restaurant.create(restaurantList)
+    .then(() => {
+      console.log("done")
+    })
+    .catch(() => {
+      console.log("error")
+    })
+})
