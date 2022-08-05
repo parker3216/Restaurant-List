b3e9b64815a254ed34dbfc559d6619a302cd6f9a
parker3216 <parker3216@hotmail.com>
2022-08-05 17:40:11 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-// 載入 express 並建構應用程式伺服器
-const express = require('express')
+const express = require('express')// 載入 express 並建構應用程式伺服器
+const exphbs = require('express-handlebars')
 const mongoose = require('mongoose') // 載入 mongoose
-mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB
+const Restaurant = require('./models/restaurant') // 載入 restaurant model
+const bodyParser = require('body-parser')
 
+mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB
 // 取得資料庫連線狀態
 const db = mongoose.connection
 // 連線異常
@@ -14,23 +16,41 @@ db.once('open', () =>{
   console.log('mongodb connected!')
 })
 
-
-
 const app = express()
 const port = 3000
 
-const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
+
+//const restaurantList = require('./restaurant.json')
 
 // set template engine
 app.engine('handlebars',exphbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true}))
 
-// set routes 
-app.get('/',(req,res) =>{
-  res.render('index',{ restaurants:restaurantList.results})
+
+
+//新增餐廳首頁
+app.get('/restaurants/new' , (req,res) =>{
+  return res.render('new')
 })
+//新增餐廳
+app.post('/restaurants', (req, res) => {
+  //onst data = req.body      // 從 req.body 拿出表單裡的資料
+  Restaurant.create(req.body)     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
+
+
+// 查看全部餐廳
+app.get('/',(req,res) =>{
+  Restaurant.find({})
+  .lean()
+  .then(restaurants => res.render('index', { restaurants }) )
+  .catch(error => console.log(error))
+})
+
 
 app.get('/restaurants/:restaurant_id',(req,res) =>{
   const restaurant = restaurantList.results.find(restaurant => restaurant.id === Number(req.params.restaurant_id))
