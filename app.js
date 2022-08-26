e853e0e9174772a7c7180a9d04180be63127a655
parker3216 <parker3216@hotmail.com>
2022-08-26 15:49:51 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')// 載入 express 並建構應用程式伺服器
+const session = require('express-session')//載入express-session
 const exphbs = require('express-handlebars')
 const Restaurant = require('./models/restaurant') // 載入 restaurant model
 const bodyParser = require('body-parser')
@@ -6,19 +7,27 @@ const methodOverride = require('method-override') // 載入 method-override
 const routes = require('./routes') //引用路由器
 require('./config/mongoose')//引用mongoose連線設定
 
-const port = 3000
+const PORT = 3000
 const app = express()
 
 
 
 app.engine('handlebars',exphbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
+
+//使用session套件
+app.use(session({
+  secret: 'GoodThing', //驗證session id的字串
+  resave: false,  //不強制把session更新到session store
+  saveUninitialized: true //將未初始化的session存回session store
+}))
+
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
 
 
-app.listen( port ,() =>{
-  console.log(`Listening on http://localhost:${port}`)
+app.listen( PORT ,() =>{
+  console.log(`Listening on http://localhost:${PORT}`)
 })
