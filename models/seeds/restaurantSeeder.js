a6ea36588cbfabbc696ec9c043144d7751110b01
parker3216 <parker3216@hotmail.com>
2022-08-27 17:23:29 +0800

    modify todoSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,22 +1,49 @@
-const Restaurant = require('../restaurant') // 載入 restaurant model
-const restaurantList = require("../../restaurant.json").results //載入清單
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
+const Restaurant = require('../restaurant') // 載入 restaurant model
+const User = require('../user')
+const restaurantList = require("../../restaurant.json").results //載入餐廳清單
+const userList = require('../../user.json').results //載入使用者
 const db = require('../../config/mongoose') // 引用mongoose連線
 
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-// 連線成功
 db.once('open', () => {
-  console.log('loading restaurantSeeder data!')
-
-  Restaurant.create(restaurantList)
-    .then(() => {
-      console.log("done")
+  Promise.all(
+    Array.from(userList, (userlist) => {
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(userlist.password, salt))
+        .then(hash => User.create({
+          name: userlist.name,
+          email: userlist.email,
+          password: hash
     })
-    .catch(() => {
-      console.log("error")
+  )
+    .then(user => {
+      const userId = user._id
+      const seedRestaurant = []
+      userlist.restaurant_index.forEach(index =>{
+        restaurantList[index].userId = userId
+        seedRestaurant.push(restaurantList[index])
+      })
+      return Restaurant.create(seedRestaurant)
     })
-})
+  })
+)
+    .then(()=>{
+      console.log('user & restaurant seeders loaded!')
+      process.exit() 
+    })
+    .catch(error => console.log(error))
+  })
+  
+  
+  
+  
+  
+  
+  
+  
+ 
